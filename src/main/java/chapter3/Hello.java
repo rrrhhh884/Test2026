3d8c59fe4aa557f8364ca5873a0b9231a4401d4b
djangouser <hayashi.0219@gmail.com>
2026-04-28 19:49:42 +0900

    修正

--- src/main/java/chapter3/Hello.java
+++ src/main/java/chapter3/Hello.java
@@ -4,11 +4,11 @@ package chapter3;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns={"/chapter3/hello"})
 public class Hello extends HttpServlet {
